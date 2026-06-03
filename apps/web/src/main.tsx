aaab863d384b72b3d775f287a38f077b877285bc
XingKaiXin <xingkaixin@gmail.com>
2026-06-03 18:08:17 +0800

    feat(perf): add benchmark profiling

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { RenderProfiler } from "./components/RenderProfiler.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RenderProfiler id="App">
+        <App />
+      </RenderProfiler>
     </BrowserRouter>
   </React.StrictMode>,
 );
